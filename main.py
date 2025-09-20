25ed3d02a9726d7dd0d0f7745d4aa731d39a2108
Solo Morgan <akili4242564@gmail.com>
2025-09-19 22:16:04 -0400

    First Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+game_name = "SlayQuest" 
+print("Welcome To SlayQuest!") # TODO, print greetings with game name
+print("=====================S") # TODO, add or remove equal symbol to align the greetings
+print("Before we begin what's your character's name?")
+ # Ask for the character's name
+name = input()
+print("Great,", name +"!","Let's begin the adventure!") 
+ # Print the name
